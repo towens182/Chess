41e79ad575bb796a3d2f0489fd8755788eca79d9
towens182 <TOwens@student.dwc.edu>
2016-09-27 12:41:57 -0400

    Start

--- Chess/Chess/Chess.cpp
+++ Chess/Chess/Chess.cpp
@@ -3,8 +3,18 @@
 //September 20, 2016
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+
 int main()
 {
+	using namespace std;
+
+	enum command = { S, M, }
+
+	cout 
+
+
 
 
     return 0;
